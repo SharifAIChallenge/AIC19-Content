6787758733913f2f8c14f37490fc818474647f73
miladmirza75 <miladmirza75@gmail.com>
2019-02-14 23:09:26 +0330

    fix bug(breadthFirstSearch should be bfs )

--- Scientific/Search/pacman/eightpuzzle.py
+++ Scientific/Search/pacman/eightpuzzle.py
@@ -268,7 +268,7 @@ if __name__ == '__main__':
     print(puzzle)
 
     problem = EightPuzzleSearchProblem(puzzle)
-    path = search.breadthFirstSearch(problem)
+    path = search.bfs(problem)
     print(('BFS found a path of %d moves: %s' % (len(path), str(path))))
     curr = puzzle
     i = 1
